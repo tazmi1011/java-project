119561bdb86bf5d56f2b9c994b5cc66d1d2e0dd7
fivefive55 <charlieyang930730@gmail.com>
2024-05-08 16:04:42 +0800

    feat: uuuuu

--- FirstClass14Team.java
+++ FirstClass14Team.java
@@ -3,5 +3,6 @@ public class FirstClass14Team
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157138\"\n姓名：\"蔡軒語\"");
+        System.out.println("\"學號：\"01157113\"\n姓名：\"楊昕諺\"");
     }
 }
