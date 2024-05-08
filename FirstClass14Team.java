ab43caebd575f2d6ea001587f89deb1153e577ac
tazmi <caix49704@gmail.com>
2024-05-08 16:07:44 +0800

    feat: yoyo

--- FirstClass14Team.java
+++ FirstClass14Team.java
@@ -4,5 +4,6 @@ public class FirstClass14Team
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157138\"\n姓名：\"蔡軒語\"");
         System.out.println("\"學號：\"01157113\"\n姓名：\"楊昕諺\"");
+         System.out.println("\"學號：\"\"\n姓名：\"\"");
     }
 }
