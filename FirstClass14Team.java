2e3b0ff7a44299627cac1b23cedcf9f9e88a9649
unknown <you@example.com>
2024-05-08 16:02:46 +0800

    feat: upload

---
+++ FirstClass14Team.java
@@ -0,0 +1,7 @@
+public class FirstClass14Team
+ {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01157138\"\n姓名：\"蔡軒語\"");
+    }
+}
